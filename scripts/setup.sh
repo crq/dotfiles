2383bdc7a524996159b6b5fa9f8a39f0abd76944
Charles Quirin <crq@users.noreply.github.com>
2016-04-18 15:23:17 -0400

    Install Sublime Babel Syntax

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -75,3 +75,5 @@ brew install caskroom/versions/sublime-text3
 ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -s ~/.dotfiles/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s ~/.dotfiles/sublime/JavaScript\ \(Babel\).sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript\ \(Babel\).sublime-settings
+ln -s ~/.dotfiles/sublime/JavaScript.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript.sublime-settings
