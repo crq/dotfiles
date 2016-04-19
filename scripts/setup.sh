59112e30fb97cc88a8e2b07fb9a0e406abb656d2
Charles Quirin <crq@users.noreply.github.com>
2016-04-18 22:25:46 -0400

    Move Sublime link commands to seperate file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -71,10 +71,4 @@ vim +PluginInstall +qall
 
 # Configure Sublime
 brew install caskroom/versions/sublime-text3
-# This part is untested - fix as necessary
-ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -s ~/.dotfiles/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -s ~/.dotfiles/sublime/JavaScript\ \(Babel\).sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript\ \(Babel\).sublime-settings
-ln -s ~/.dotfiles/sublime/JavaScript.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript.sublime-settings
-ln -s ~/.dotfiles/sublime/snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
+source ~/.dotfiles/scripts/sublime_setup.sh
---
+++ scripts/sublime_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set up links for Sublime
+ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s ~/.dotfiles/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s ~/.dotfiles/sublime/JavaScript\ \(Babel\).sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript\ \(Babel\).sublime-settings
+ln -s ~/.dotfiles/sublime/JavaScript.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript.sublime-settings
+ln -s ~/.dotfiles/sublime/snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
