ad94b7e868ee4952bdf697e50fd3398c69427429
Charles Quirin <crq@users.noreply.github.com>
2016-06-19 12:39:37 -0400

    Sublime: Settings for Elixir syntax

--- scripts/sublime_setup.sh
+++ scripts/sublime_setup.sh
@@ -4,7 +4,11 @@
 ln -sF ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -sF ~/.dotfiles/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 ln -sF ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+# Links for Syntax settings
 ln -sF ~/.dotfiles/sublime/JavaScript\ \(Babel\).sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript\ \(Babel\).sublime-settings
 ln -sF ~/.dotfiles/sublime/JavaScript.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript.sublime-settings
+ln -sF ~/.dotfiles/sublime/Elixir.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Elixir.sublime-settings
+
 ln -sF ~/.dotfiles/sublime/snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 ln -sF ~/.dotfiles/sublime/macros ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
