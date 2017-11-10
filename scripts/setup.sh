a8e965f8862b54ace5b5fb634a9225141a17f252
Charles Quirin <crq@users.noreply.github.com>
2017-11-09 23:04:44 -0500

    VIM: Update setup for vim

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,6 @@ mkdir ~/bin
 # Copy fonts
 cp ~/.dotfiles/fonts/hack/v3/ttf/*.ttf ~/Library/Fonts/
 
-
 # Link files
 ln -sF ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sF ~/.dotfiles/.gitignore_global ~/.gitignore_global
@@ -23,7 +22,6 @@ brew update
 
 # Install Common Homebrew packages
 brew install git
-brew install caskroom/cask/brew-cask
 brew install gpg2
 brew install ctags
 brew install cmake
@@ -33,12 +31,11 @@ brew install n
 
 # Configure and Install VIM
 brew install macvim --with-override-system-vim --with-cscope
-# Links macvim to Applications folder
-brew linkapps macvim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-# Compile YouCompleteMe VIM plugin
-~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
 # Configure Sublime
 source ~/.dotfiles/scripts/sublime_setup.sh
+
+# Disable Apple press and hold (gets you key repeat in Sublime)
+defaults write -g ApplePressAndHoldEnabled -bool false
