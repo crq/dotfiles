49cfc24b119c6ad0e7673974e6c0ff537e89f959
Charles Quirin <crq@users.noreply.github.com>
2017-11-09 21:10:39 -0500

    Scripts: Reorganzie and remove packages that are not required

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,6 @@ ln -sF ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -sF ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sF ~/.dotfiles/.vimrc ~/.vimrc
 ln -sF ~/.dotfiles/.gvimrc ~/.gvimrc
-ln -sF ~/.dotfiles/.tmux.conf ~/.tmux.conf
 mkdir ~/.config
 ln -sF ~/.dotfiles/base16-shell/ ~/.config/base16-shell
 ln -sF ~/.dotfiles/.bundle/ ~/.bundle
@@ -18,28 +17,17 @@ ln -sF ~/.dotfiles/.bundle/ ~/.bundle
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 
-# Configure Git
+# Install Common Homebrew packages
 brew install git
-
-source ~/.dotfiles/scripts/postgres_setup.sh
-
-# Install Homebrew packages
 brew install caskroom/cask/brew-cask
 brew install gpg2
-brew install tmux
 brew install ctags
 brew install cmake
 brew install the_silver_searcher
 brew install bash-completion
 brew install n
-brew cask install iterm2
-brew cask install google-chrome
 
-source ~/.dotfiles/scripts/ruby_setup.sh
-source ~/.dotfiles/scripts/php_setup.sh
-source ~/.dotfiles/scripts/python_setup.sh
-
-# Configure VIM
+# Configure and Install VIM
 brew install macvim --with-override-system-vim --with-cscope
 # Links macvim to Applications folder
 brew linkapps macvim
@@ -49,5 +37,4 @@ vim +PluginInstall +qall
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
 # Configure Sublime
-brew install caskroom/versions/sublime-text3
 source ~/.dotfiles/scripts/sublime_setup.sh
