f9e7053ce5dfe271ff146f9bcca12fadc67a9234
Charles Quirin <crq@users.noreply.github.com>
2018-01-16 11:39:56 -0500

    VSCode: Add snippets for Jest

--- scripts/vscode_setup.sh
+++ scripts/vscode_setup.sh
@@ -1,2 +1,3 @@
 # Set up links for VSCode
 ln -sF ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sF ~/.dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/
