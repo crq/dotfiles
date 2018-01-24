3613f0b2e309e523fa2932fcc9fda9c4ca3c6dac
Charles Quirin <crq@users.noreply.github.com>
2018-01-23 20:57:51 -0500

    Zsh: Check in custom directory excluding plugins folder

--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -11,6 +11,9 @@ sudo chsh -s $(which zsh)
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+rm -rf ~/.oh-my-zsh/custom
+ln -sF ~/.dotfiles/oh-my-zsh/custom ~/.oh-my-zsh/custom
+
 # Install autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
